f84acaa4a01c27d92270d3b21d36bfe42cdd4285
Gabe <gabrielgrechuk@outlook.com>
2026-08-25 23:48:54 -0300

    #3 - Reuse MikroORM configuration file for services runtime

--- libs/database/src/mikro-orm.config.ts
+++ libs/database/src/mikro-orm.config.ts
@@ -1,20 +1,26 @@
-import { defineConfig } from "@mikro-orm/postgresql";
-import { Migrator } from "@mikro-orm/migrations";
-import { WalletSchema } from "./entities/wallet.entity";
+import { defineConfig } from '@mikro-orm/postgresql';
+import { Migrator } from '@mikro-orm/migrations';
+import { WalletSchema } from './entities/wallet.entity';
+
+// Yep, should be true as far as it's told to be false
+const mikroormDebugMode =
+  process.env.MIRKO_ORM_DEBUG_MODE === 'false' ? false : true;
 
 export const config = defineConfig({
-  dbName: "wagering_db",
+  // Not in a env variable because this config should be usable by the migratuon
+  // scripts.
+  dbName: 'wagering_db',
   extensions: [Migrator],
 
   migrations: {
-    path: "./libs/database/migrations",
-    pathTs: "./libs/database/migrations",
+    path: './libs/database/migrations',
+    pathTs: './libs/database/migrations',
   },
 
   entities: [WalletSchema],
 
   // TODO: Remove.
-  debug: true,
+  debug: mikroormDebugMode,
 });
 
 export default config;
