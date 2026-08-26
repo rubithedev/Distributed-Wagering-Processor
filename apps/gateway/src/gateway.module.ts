2b0c3656e4fdb5a498137e65c6788c50f0f41f4e
Gabe <gabrielgrechuk@outlook.com>
2026-08-26 16:03:07 -0300

    #5 - Add config service and swagger auto-generated docs

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { GatewayController } from './gateway.controller';
 import { GatewayService } from './gateway.service';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        APP_NAME: Joi.string().required(),
+
+        MIRKO_ORM_DEBUG_MODE: Joi.string().required(),
+        POSTGRES_SSH_CONNECTION: Joi.string().required(),
+        POSTGRES_CONNECTION_STRING: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+
+        AWS_REGION: Joi.string().required(),
+        AWS_ACCESS_KEY_ID: Joi.string().required(),
+        AWS_SECRET_ACCESS_KEY: Joi.string().required(),
+        SQS_ENDPOINT: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [GatewayController],
   providers: [GatewayService],
 })
--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,9 +1,55 @@
 import { NestFactory } from '@nestjs/core';
 import { GatewayModule } from './gateway.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { writeFileSync } from 'fs';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const port = process.env.GATEWAY_PORT || 3000;
-  const app = await NestFactory.create(GatewayModule);
+
+  const configApiDoc = new DocumentBuilder()
+    .setTitle('Distributed Wagering Processor')
+    .setDescription('The Gateway API')
+    .setVersion('0.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth',
+    )
+    .addServer('http://localhost:{PORT}', 'Local Environment', {
+      PORT: { default: port },
+    })
+    .build();
+
+  const app = await NestFactory.create(GatewayModule, {
+    // Allow everything from everywhere.
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    },
+  });
+
+  if (process.env.ENABLE_API_DOC === 'true') {
+    const document = SwaggerModule.createDocument(app, configApiDoc);
+    writeFileSync('./docs/openapi.json', JSON.stringify(document));
+    SwaggerModule.setup('api-docs', app, document);
+  }
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
   await app.listen(port, () => {
     console.log('Gateway listening to:', port);
   });
